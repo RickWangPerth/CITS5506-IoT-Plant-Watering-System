0ba76cf59236bd9704ebae629ce02e337504bddf
Raspberry Pi <cits5506@raspberrypi>
2022-09-23 09:34:09 +0800

    Fixed indentation

--- app/sensors/collect_sensors.py
+++ app/sensors/collect_sensors.py
@@ -8,33 +8,33 @@ if platform == "linux":
     from app.models import History
     from datetime import datetime
 
-    class CollectSensors:
-        moisture = None
-        temperature = None
-        water_present = None
+class CollectSensors:
+    moisture = None
+    temperature = None
+    water_present = None
 
-        def __init__(self, db):
-            self.db = db
+    def __init__(self, db):
+        self.db = db
 
-            # Analog I/O
-            self.REF_VOLTAGE = 4.096
-            self.adc = ExpanderPi.ADC()  # create an instance of the ADC
-            self.adc.set_adc_refvoltage(self.REF_VOLTAGE)
-            self.MOISTURE_PIN = 2
-            self.TEMPERATURE_PIN = 1
+        # Analog I/O
+        self.REF_VOLTAGE = 4.096
+        self.adc = ExpanderPi.ADC()  # create an instance of the ADC
+        self.adc.set_adc_refvoltage(self.REF_VOLTAGE)
+        self.MOISTURE_PIN = 2
+        self.TEMPERATURE_PIN = 1
 
-            # Digital I/O
-            self.WATER_LEVEL_PIN = 3
-            self.io = ExpanderPi.IO()
+        # Digital I/O
+        self.WATER_LEVEL_PIN = 3
+        self.io = ExpanderPi.IO()
 
-            # Camera
-            self.PHOTOS_FOLDER = '/home/cits5506/plant_photos'
-            self.KEEP_PHOTOS = 10
+        # Camera
+        self.PHOTOS_FOLDER = '/home/cits5506/plant_photos'
+        self.KEEP_PHOTOS = 10
 
-            self.camera = Camera(self.PHOTOS_FOLDER, self.KEEP_PHOTOS)
-            self.moisture_sensor = MoistureSensor(self.adc, self.REF_VOLTAGE, self.MOISTURE_PIN)
-            self.temperature_sensor = TemperatureSensor(self.adc, self.TEMPERATURE_PIN)
-            self.water_level_sensor = WaterLevelSensor(self.io, self.WATER_LEVEL_PIN)
+        self.camera = Camera(self.PHOTOS_FOLDER, self.KEEP_PHOTOS)
+        self.moisture_sensor = MoistureSensor(self.adc, self.REF_VOLTAGE, self.MOISTURE_PIN)
+        self.temperature_sensor = TemperatureSensor(self.adc, self.TEMPERATURE_PIN)
+        self.water_level_sensor = WaterLevelSensor(self.io, self.WATER_LEVEL_PIN)
 
         # Get initial readings
         self.update()

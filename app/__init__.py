b629a16e4cd2f64a9d0bd5ba27ea3ccabfdb4f01
Raspberry Pi <cits5506@raspberrypi>
2022-09-30 13:38:48 +0800

    Close camera connection

--- app/__init__.py
+++ app/__init__.py
@@ -13,8 +13,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-from app import routes, models
-
 if platform == "linux":
     from app.sensors.collect_sensors import CollectSensors
     import atexit
@@ -26,9 +24,15 @@ if platform == "linux":
     scheduler.add_job(func=collect_sensors.update_database, trigger="interval", seconds=3)
     scheduler.start()
 
-    # Shut down the scheduler when exiting the app
-    atexit.register(lambda: scheduler.shutdown())
+    def shutdown():
+        print("Stopping...")
+        collect_sensors.camera.camera.close()
+        scheduler.shutdown()
 
+    # Shut down the scheduler when exiting the app
+    atexit.register(shutdown)
+
+from app import routes, models
 
 @app.shell_context_processor
 def make_shell_context():
